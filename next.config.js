2b0b7a14e3246e1c3c13f44f66edace33f662a16
tamagotchiprim3 <webmishin.a.s@gmail.com>
2023-04-15 11:50:49 +0300

    change api key

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    REACT_APP_API_KEY: "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",
-  },
 };
 
 module.exports = nextConfig;
