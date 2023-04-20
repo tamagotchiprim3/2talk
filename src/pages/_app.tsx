8c732cd7266b16701c4fc1f2eecb85f50196c5ce
tamagotchiprim3 <webmishin.a.s@gmail.com>
2023-04-20 10:04:02 +0300

    add vercel-analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} className="w-full h-full " />;
+  return (
+    <>
+      <Component {...pageProps} className="w-full h-full " />;
+      <Analytics />
+    </>
+  );
 }
 
